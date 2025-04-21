2b95d8063d656d89408597c919f5d6aed936f287
Nilkanth Thakkar <166748431+Nilkanth-ai@users.noreply.github.com>
2025-04-21 10:57:31 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,23 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
-
-const Part = ({ name, exercises }) => {
-  return <p>{name} {exercises}</p>
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  return (
-    <p>
-      Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
-    </p>
-  )
-}
+import { useState } from 'react'
+import Button from './components/Button'
+import Statistics from './components/Statistics'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Give Feedback</h1>
+      <Button text="Good" onClick={handleGood} />
+      <Button text="Neutral" onClick={handleNeutral} />
+      <Button text="Bad" onClick={handleBad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
